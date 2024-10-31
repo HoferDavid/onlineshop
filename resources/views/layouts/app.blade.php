c9bb042200ea603bd3d1c07c58bf00cfb39eeffd
David Hofer <chassis.poppets0x@icloud.com>
2024-10-31 17:44:05 +0100

    feat: add HomeController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,5 @@
     <x-layouts.header />
 
     {{ $slot }}
-
-    <footer>
-        <a href="#">link 1</a>
-        <a href="#">link 2</a>
-        {{ $footerLinks }}
-    </footer>
+    
 </x-base-layout>
